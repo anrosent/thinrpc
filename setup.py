5ad177ac9a4a3438690905d0e8e3f4da689a3ab2
anrosent <Anson_Rosenthal@brown.edu>
2015-06-04 17:25:45 -0400

    prep for dist to pull apps out of here

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import os.path
+
+README = os.path.join(os.path.dirname(__file__), 'README')
+
+version = '0.1.4'
+
+with open(README) as fp:
+    longdesc = fp.read()
+
+setup(name='thinrpc',
+    version=version,
+    description='Thin RPC framework',
+    long_description=longdesc,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+        'Intended Audience :: Developers'
+    ],
+    author='Anson Rosenthal',
+    author_email='anson.rosenthal@gmail.com',
+    license='MIT License',
+    url='https://github.com/anrosent/pyrpc.git',
+    py_modules=['pyrpc'],
+)
+
